a6331b864e79169c8f102374c3f62e54f6ae80ba
deenos25 <54684272+deenos25@users.noreply.github.com>
2023-02-08 16:31:51 -0600

    Add files via upload

---
+++ window_average.py
@@ -0,0 +1,20 @@
+import numpy as np
+def wind_avg(window_size, stride, X, X_feat_num, Y, Y_feat_num):
+    ##############
+    #print('Overlap:', 100 * (window_size - stride) / window_size)
+    X1, Y1 = np.array([]), np.array([])
+    for f in range(0,X_feat_num):
+        X2 = [np.mean(X[:,f][i:i+window_size]) for i in range(0, len(X[:,f]), stride) if i+window_size <= len(X[:,f]) ]
+        if f == 0:
+            X1 = np.append(X1, [X2])
+        else:
+            X1 = np.vstack([X1, X2])
+    for g in range(0,Y_feat_num):
+        Y2 = [np.mean(Y[:, g][i:i + window_size]) for i in range(0, len(Y[:, g]), stride) if i + window_size <= len(Y[:, g])]
+        if g == 0:
+            Y1 = np.append(Y1, [Y2])
+        else:
+            Y1 = np.vstack([Y1, Y2])
+    Y = Y1.T
+    X = X1.T
+    return X, Y
